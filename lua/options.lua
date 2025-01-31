857cf23a8979bd5de4a26189417fec37791e07ba
Nestos0 <trudav1830@outlook.com>
2025-01-31 21:23:04 +0800

    feat:docker-compose filetype

--- lua/options.lua
+++ lua/options.lua
@@ -33,8 +33,8 @@ o.numberwidth = 1
 o.ruler = false
 
 -- disable nvim intro
-opt.shortmess:append "sI"
-opt.shortmess:remove "l"
+opt.shortmess:append("sI")
+opt.shortmess:remove("l")
 
 o.signcolumn = "yes"
 o.splitbelow = true
@@ -47,7 +47,7 @@ o.updatetime = 250
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
-opt.whichwrap:append "<>hl"
+opt.whichwrap:append("<>hl")
 
 -- disable some default providers
 g.loaded_node_provider = 0
@@ -56,7 +56,16 @@ g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
 
 -- add binaries installed by mason.nvim to path
-local is_windows = vim.fn.has "win32" ~= 0
+local is_windows = vim.fn.has("win32") ~= 0
 local sep = is_windows and "\\" or "/"
 local delim = is_windows and ";" or ":"
-vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
+vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH
+
+vim.filetype.add({
+	pattern = {
+		["docker-compose%.yml"] = "yaml.docker-compose",
+		["docker-compose%.yaml"] = "yaml.docker-compose",
+		["compose%.yml"] = "yaml.docker-compose",
+		["compose%.yaml"] = "yaml.docker-compose",
+	},
+})
