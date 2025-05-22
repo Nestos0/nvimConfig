5348cd48a53b1b8a1ec80a3c40cd958e38096898
Nestos0 <trudav1830@outlook.com>
2025-05-22 22:34:08 +0800

    update

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
+Lazy = {}
+require("plugins/lsp/lsp")
 require("config.lazy")
 require("config.colorscheme")
+require("autocmds")
 require("options")
 require("mappings")
--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,23 +1,23 @@
 vim.api.nvim_create_autocmd("User", {
-    pattern = "IceStart",
-    once = true,
-    callback = function()
-        local function _trigger()
-            vim.api.nvim_exec_autocmds("User", { pattern = "IceLoad" })
-        end
+  pattern = "IceStart",
+  once = true,
+  callback = function()
+    local function _trigger()
+      vim.api.nvim_exec_autocmds("User", { pattern = "IceLoad" })
+    end
 
-        if vim.bo.filetype == "snacks_dashboard" then
-            vim.api.nvim_create_autocmd("BufEnter", { pattern = "*/*", once = true, callback = _trigger })
-        else
-            _trigger()
-        end
-    end,
+    if vim.bo.filetype == "snacks_dashboard" then
+      vim.api.nvim_create_autocmd("BufEnter", { pattern = "*/*", once = true, callback = _trigger })
+    else
+      _trigger()
+    end
+  end,
 })
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
+vim.api.nvim_create_autocmd("BufReadPre", {
+  pattern = "*",
   once = true,
-  callback = function() 
-      vim.api.nvim_exec_autocmds("User", { pattern = "IceStart" })
-  end
+  callback = function()
+    vim.api.nvim_exec_autocmds("User", { pattern = "IceStart" })
+  end,
 })
