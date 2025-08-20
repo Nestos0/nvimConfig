9f81bdfcc35fd7f0889d255eefba07d1de97b334
Nestos0 <trudav1830@outlook.com>
2025-08-21 01:15:27 +0800

    update():

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -1,18 +1,26 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim"
-    },
-    config = function()
-      local null_ls = require("null-ls")
+  "mfussenegger/nvim-lint",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+  config = function()
+    local lint = require("lint")
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.completion.spell,
-          -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
-        },
-      })
-    end,
-  },
+    lint.linters_by_ft = {
+      c = { "cpplint" },
+      cpp = { "cpplint" },
+    }
+
+    lint.linters.cpplint.args = {
+      "--filter=-legal/copyright",
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
 }
