b2fab3674077a524c202cceb194bf685f6fa50fb
Nestos <trudav1830@outlook.com>
2025-12-19 22:09:49 +0800

    Sync():

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -12,6 +12,12 @@ return {
         "conf",
         -- 如果您自定义了全局对象，也放在这里
       },
+      workspace = {
+        library = {
+          [vim.fn.expand("$HOME/.local/share/nvim/mason/packages/lua-language-server/libexec/meta/3rd/love2d")] = true,
+        },
+        checkThirdParty = false,
+      },
     },
   },
 }
