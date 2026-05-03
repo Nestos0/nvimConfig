f4b71a711c7b71841e1c1b0a59b8883ab44c2272
Nestos <trudav1830@outlook.com>
2026-05-03 12:42:54 +0800

    update

--- lua/plugins/specialized.lua
+++ lua/plugins/specialized.lua
@@ -96,10 +96,10 @@ return {
       vim.o.autoread = true -- Required for `opts.events.reload`
 
       -- Recommended/example keymaps
-      vim.keymap.set({ "n", "x" }, "<C-a>", function()
+      vim.keymap.set({ "n", "x" }, "<A-a>", function()
         require("opencode").ask("@this: ", { submit = true })
       end, { desc = "Ask opencode…" })
-      vim.keymap.set({ "n", "x" }, "<C-x>", function()
+      vim.keymap.set({ "n", "x" }, "<A-x>", function()
         require("opencode").select()
       end, { desc = "Execute opencode action…" })
       vim.keymap.set({ "n", "t" }, "<C-.>", function()
